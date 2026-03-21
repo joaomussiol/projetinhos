9ae817da2fd534e9176419d1d9180ddc08478446
Joao Mussiol <joao_mussiol@estudante.sesisenai.org.br>
2026-03-21 20:14:32 -0300

    Calculadora Versão 6. Agora com Def, e try e except

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,36 @@
-while True:
-    num1 = float(input("Digite o primeiro numero: "))
-    operador = input("Digite um operador. Ele pode ser. +, -, *, / : ")
-    num2 = float(input("Digite o segundo numero: "))
-    resultado = None
-
-    if operador == "+" :
-        resultado = num1 + num2
-
-    elif operador == "-" :
-        resultado = num1 - num2
-
-    elif operador == "/" :
-        if num2 == 0:
-            print("ERRO, DIVISÃO POR ZERO")
+def calcular(num1, operador, num2):
+        if operador == "+" :
+            return num1 + num2
+    
+        elif operador == "-" :
+            return num1 - num2
+    
+        elif operador == "*" :
+            return num1 * num2
+    
+        elif operador == "/":
+            if num2 == 0:
+                print("ERRO, DIVISÃO POR ZERO")
+                return None
+            else:
+                return num1 / num2
         else:
-            resultado = num1 / num2
+            print("Operador inválido")
+            return None
 
-    elif operador == "*" :
-        resultado = num1 * num2
+while True:
+    try:
+        num1 = float(input("Digite o primeiro numero: "))
+        operador = input("Digite um operador. Ele pode ser. +, -, *, / : ")
+        num2 = float(input("Digite o segundo numero: "))
 
-    else:
-        print("Operador invalido")
+    except ValueError:
+        print("Insira apenas valores corretos")
+        continue
+    resultado = calcular(num1, operador, num2)
 
     if resultado is not None:
-        print(f"{num1} {operador} {num2} = {round(resultado, 3)}")
+        print(f"{num1} {operador} {num2} = {resultado:.3f}")
 
     continuar = input("Deseja continuar? (S/N): ").lower().strip()
 
